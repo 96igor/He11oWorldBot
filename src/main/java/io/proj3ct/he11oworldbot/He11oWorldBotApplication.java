6b1dd8bde0e671805f9244e3e1b2d6a3e6f0ba48
Игорь Царёв <tsarov88@gmail.com>
2025-03-13 18:37:05 +0200

    initial commit

---
+++ src/main/java/io/proj3ct/he11oworldbot/He11oWorldBotApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.he11oworldbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class He11oWorldBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(He11oWorldBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/proj3ct/he11oworldbot/He11oWorldBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.he11oworldbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class He11oWorldBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
